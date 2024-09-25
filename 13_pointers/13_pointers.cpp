8dc92685e4d002fa96b592dab3e8fb874d155e72
konopelko_d <konopelko_d@itstep.academy>
2024-09-25 19:15:09 +0300

    pointers

--- 13_pointers/13_pointers.cpp
+++ 13_pointers/13_pointers.cpp
@@ -4,101 +4,99 @@ using namespace std;
 
 void Swap(int* a, int* b)
 {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+    cout << "swap address a :: " << a << endl;
+    cout << "swap address b :: " << b << endl;
 }
 
-void min_max(int arr[], size_t length, int* min, int* max)
+void min_max(int arr[], size_t length, int* out_min, int* out_max)
 {
-	*min = arr[0];
-	*max = arr[0];
+    *out_min = arr[0];
+    *out_max = arr[0];
+    for (size_t i = 1; i < length; i++)
+    {
+        if (*out_min > arr[i]) {
+            *out_min = arr[i];
+        }
+        if (*out_max < arr[i]) {
+            *out_max = arr[i];
+        }
+    }
 
-	for (size_t i = 1; i < length; i++)
-	{
-		if (*min > arr[i])
-		{
-			*min = arr[i];
-		}
-		if (*max < arr[i])
-		{
-			*max = arr[i];
-		}
-	}
 }
+void print(int arr[], size_t length, string prompt = "")
+{
+    if (!prompt.empty())
+        cout << prompt << " :: ";
+    for (size_t i = 0; i < length; i++)
+    {
+        cout << arr[i] << "\t";
+    }cout << endl;
+}
+
 //Написати програму, що містить функцію, яка приймає як аргумент покажчики на три масиви та розмір масивів, заносить у масив C суми елементів масивів А і В.Розмір масивів однаковий.
-void task(int *a, int *b, int * c, size_t size)
+void fillC(int* a, int* b, int* c, size_t length)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		//c[i] = 
-		*(c + i) = *(a + i) + *(b + i);
-	}
+    /*int* ptr_a = a, * ptr_b = b, * ptr_c = c;*/
+    for (size_t i = 0; i < length; i++)
+    {
+        *c = *a + *b;
+        c++;
+        a++;
+        b++;
+        //*(c + i) = *(a + i) + *(b + i);
+    }
 }
 int main()
 {
-	int a = 15, b = 7;
-	int* pointer_a = &a;
-	int* pointer_b = &b;
-	cout << "Address A :: " << pointer_a << endl;
-	cout << "Address B :: " << pointer_b << endl;
-	cout << "\n ==================== Test swap ==================" << endl;
-	cout <<  "a :: " << a << "\t b :: " << b << endl;
-	Swap(pointer_a, &b);
-	cout << "a :: " << a << "\t b :: " << b << endl;
+    int a = 5, b = 10;
+    int* pointer_a = &a;
+    int* pointer_b = &b;
+    cout << "main address a :: " << pointer_a << endl;
+    cout << "main address b :: " << pointer_b << endl;
+    cout << "a :: " << a << "\t b :: " << b << endl;
+    Swap(pointer_a, &b);
+    cout << "a :: " << a << "\t b :: " << b << endl;
 
-	const int size = 10;
-	int arr[size]{ 1,5,4,7,8,5,9,6,5,4 };
-	for (size_t i = 0; i < size; i++)
-	{
-		cout << arr[i] << "\t";
-	}cout << endl;
-	int min, max;
-	min_max(arr, size, &min, &max);
-	cout << "min :: " << min << endl;
-	cout << "max :: " << max << endl;
-	
-	cout << "Test array address --> " << endl;
+    const int size = 10;
+    int arr[size]{ 4,9,47,8,5,4,8,5,2,1 };
+    print(arr, size);
+    int min, max;
+    min_max(arr, size, &min, &max);
+    cout << "min :: " << min << endl;
+    cout << "max :: " << max << endl;
 
-	cout << "Address cell 1 " << arr << "\t Value :: " << *arr << endl;
-	cout << "Address cell 2 " << arr + 1 << "\t Value :: " << *(arr+1) << endl;
-	cout << "Address cell 2 " << arr + 2 << "\t Value :: " << *(arr+2) << endl;
-	cout << "Address cell 2 " << arr + 3 << "\t Value :: " << *(arr+3) << endl;
-	cout << "Address cell 2 " << arr + 4 << "\t Value :: " << *(arr+4) << endl;
-	int* ptr = arr;
-	cout << "\n\n";
-	for (size_t i = 0; i < size; i++)
-	{
-		cout << "address :: " << ptr << " \t value :: " << *ptr << endl;
-		ptr++;
-	}
+    cout << "address arr :: " << arr + 0 << " \t value :: " << *(arr + 0) << endl;
+    cout << "address arr :: " << arr + 1 << " \t value :: " << *(arr + 1) << endl;
+    cout << "address arr :: " << arr + 2 << " \t value :: " << *(arr + 2) << endl;
+    cout << "address arr :: " << arr + 3 << " \t value :: " << *(arr + 3) << endl;
 
-	cout << "\n\n";
-	for (size_t i = 0; i < 30; i++)
-	{
-		ptr--;
-		cout << "address :: " << ptr << " \t value :: " << *ptr << endl;
-	}
-	int a_[size]{}, b_[size]{}, c[size];
+    cout << "address arr :: " << arr + 33 << " \t value :: " << *(arr + 33) << endl;
 
-	for (size_t i = 0; i < size; i++)
-	{
-		*(a_ + i) = rand() % 10;
-		*(b_ + i) = rand() % 10;
-	}
-	for (size_t i = 0; i < size; i++)
-	{
-		cout << a_[i] << "\t";
-	}cout << endl;
-	for (size_t i = 0; i < size; i++)
-	{
-		cout << b_[i] << "\t";
-	}cout << endl;
-	task(a_, b_, c, size);
-	for (size_t i = 0; i < size; i++)
-	{
-		cout << c[i] << "\t";
-	}cout << endl;
+    int* ptr = arr;
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << *ptr << "\t";
+        ptr++;
+    }
+    cout << endl;
+    for (size_t i = 0; i < size; i++)
+    {
+        ptr--;
+        cout << *ptr << "\t";
+    }
+    cout << endl;
+    int A[size], B[size], C[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        A[i] = rand() % 10;
+        B[i] = rand() % 10;
+    }
+    cout << endl;
+    print(A, size, "Print array A ");
+    print(B, size, "Print array B ");
+    fillC(A, B, C, size);
+    print(C, size, "Print array C ");
 }
-
-
